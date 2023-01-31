5aa6999c523259e1a88a15fd3158aad732b8eed7
Ivan-Cherkes <skyzn60@gmail.com>
2023-01-31 12:27:01 +0100

    Presentation 1 31.01.2023

--- app/src/main/java/com/example/shoppinglist_new_course/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist_new_course/presentation/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.shoppinglist_new_course.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.example.shoppinglist_new_course.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.shopList.observe(this) {
+            Log.d("MyLog", it.toString())
+        }
+        viewModel.getShopList()
     }
 }
